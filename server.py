cc47e503e4da3b935b9eacbf1a96518903096066
Nawaphon_Luthapak <154293106+Somji25@users.noreply.github.com>
2025-07-25 12:44:25 +0700

    Update server.py

--- server.py
+++ server.py
@@ -54,4 +54,6 @@ def upload_image():
         return jsonify({"message": "❌ Failed to publish"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
